041ae0160946b639be9eeaba39a932f224c37258
mrgreenxgreen <31266446+mrgreenxgreen@users.noreply.github.com>
2017-08-23 14:53:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -154,13 +154,6 @@ else{
     </div>
   </div>
 </div>
-<script type="text/javascript" src="js/jquery.1.11.1.js"></script> 
-<script type="text/javascript" src="js/bootstrap.js"></script> 
-<script type="text/javascript" src="js/SmoothScroll.js"></script> 
-<script type="text/javascript" src="js/nivo-lightbox.js"></script> 
-<script type="text/javascript" src="js/jquery.isotope.js"></script> 
-<script type="text/javascript" src="js/jqBootstrapValidation.js"></script> 
-<script type="text/javascript" src="js/contact_me.js"></script> 
-<script type="text/javascript" src="js/main.js"></script>
+
 </body>
 </html>
